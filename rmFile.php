dcdfb06109bd3631eaad414e908982694af4d559
lurbano <lurbano@earthsciweb.org>
2023-02-19 11:52:17 -0600

    file uploads

--- rmFile.php
+++ rmFile.php
@@ -1,16 +1,29 @@
 <?php
 
-    echo 'rmFile: ';
 
     // Get POST data (json)
     $json = file_get_contents('php://input');
     // Converts it into a PHP object
     $data = json_decode($json);
 
-    echo json_encode($data);
+    // echo json_encode($data);
+    // echo json_encode($data->name);
     //echo json_encode($_FILES);
 
-    $upDir = "./uploads";
+    $upDir = "/var/www/html/MakerspaceRequests/uploads/";
+    $fname = $upDir . $data->name;
+    //echo $fname;
+
+    //echo 'rmFile: ';
+
+    if ($data->action == "remove"){
+        $a = 1;
+        //echo $fname;
+        unlink($fname);
+        echo "Removed " . $data->name;
+    }
+
+    
 
     // // get information from the url parameters if one of the parameters in called 'name'
     // if (!empty($_GET['picoID'])){
